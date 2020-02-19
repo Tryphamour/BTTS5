d3763d1494bc896177ac546afe3fa64d4c3191dd
Tryphamour <felix.gaillard51@gmail.com>
2020-02-19 10:08:43 +0100

    Group results

--- angular/src/app/mock-groups.ts
+++ angular/src/app/mock-groups.ts
@@ -3,82 +3,82 @@ import { Group } from './group';
 export const GROUPS: Group[] = [
     { 
       group_name: 'Group AYAYA',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Malox', 'Loliezi1337', 'Hanik', 'BitDust'],
+      points: ['12', '8', '6', '1']
     },
     { 
       group_name: 'Group BlessRNG',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Arbusion', 'SuperSquirrel51', 'TorreManias', 'wawrzyn9'],
+      points: ['12', '10', '7', '2']
     },
     { 
       group_name: 'Group Choke',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Maou', 'Olii', 'Erkfir', 'HiddenDenki'],
+      points: ['12', '9', '5', '2']
     },
     { 
       group_name: 'Group Don',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Blujae', 'Champs de ble', 'Geromu Keretem', 'DieButterStulle'],
+      points: ['12', '10', '4', '2']
     },
     { 
-      group_name: 'Group ETs',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      group_name: 'Group :eh:',
+      players: ['awdse22', 'BadAimBoi', 'SergoM4444', 'Lazereed'],
+      points: ['12', '8', '7', '2']
     },
     { 
       group_name: 'Group Flashlight',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Yeong Yuseong', 'Katsumi', 'Senko-san', 'Corruptyouth'],
+      points: ['12', '11', '6', '0']
     },
     { 
       group_name: 'Group Gamers',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Megafan', '-Storm7-', 'Dohland', 'Zidyx'],
+      points: ['12', '10', '7', '0']
     },
     { 
       group_name: 'Group Hype',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['TLQ_Yoshii', 'Dartandr', 'poppasanas', 'Ristpek'],
+      points: ['12', '11', '7', '0']
     },
     { 
       group_name: 'Group Idols',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Santeri', 'DrugManager', 'Pe6eHoK98', 'claggole'],
+      points: ['12', '9', '5', '1']
     },
     { 
       group_name: 'Group Jackpot',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Samoyed', 'Vaf', 'poing', 'Xellander'],
+      points: ['12', '9', '5', '3']
     },
     { 
       group_name: 'Group Kat',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Kozzo', 'Vasyadogs', 'yaityboi', 'persik23'],
+      points: ['12', '9', '3', '1']
     },
     { 
       group_name: 'Group Ladies',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['moosepi', 'Boaz', 'AhzekAhriman', '0CitrineQueen0'],
+      points: ['12', '10', '4', '1']
     },
     { 
       group_name: 'Group Mocha',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['helios44', 'ByTiger', 'NutAndGo', '-[GuiboxFR]-'],
+      points: ['12', '9', '8', '1']
     },
     { 
       group_name: 'Group NotLikeThis',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['pickxarro', 'zd101nb', 'Ullei', '-jordan-'],
+      points: ['12', '11', '4', '0']
     },
     { 
       group_name: 'Group OwO',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['Kirross', 'squidstain', 'quadrillionaire', 'Fre3Styl3r'],
+      points: ['12', '11', '6', '0']
     },
     { 
       group_name: 'Group PP',
-      players: ['John Doe', 'Cedric Gedusor', 'Sacha Palette', 'Marc Raiffe'],
-      points: ['500', '460', '320', '210']
+      players: ['DrDazzle', 'Enohx', 'Aidown', 'shineasone'],
+      points: ['12', '11', '7', '1']
     },
 ];
--- angular/src/app/mock-lobbies.ts
+++ angular/src/app/mock-lobbies.ts
@@ -1,8 +1,8 @@
 import { Lobby } from './lobby';
 
 export const LOBBIES: Lobby[] = [
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+  { pseudo: ['squidstain', 'Pickxarro'], 
+    img: ['../../assets/images/playerspfp/squidstain.jpg', 'https://via.placeholder.com/75'],
     referee: 'LadyLag'
   },
   { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
