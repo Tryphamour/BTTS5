3b76b304c1bd94a68c357e8aba70d41a25c62456
Tryphamour <felix.gaillard51@gmail.com>
2020-02-19 16:31:58 +0100

    Add players pfp in quals lobbies

--- angular/src/app/mock-lobbies.ts
+++ angular/src/app/mock-lobbies.ts
@@ -4,97 +4,97 @@ import {
 
 export const LOBBIES: Lobby[] = [{
     pseudo: ['squidstain', 'Pickxarro'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/squidstain.jpg', '../../assets/images/playerspfp/Pickxarro.jpg'],
     referee: 'LadyLag',
     date: ['20th Jan - 14:00 UTC']
   },
   {
     pseudo: ['Champs de ble'],
-    img: ['https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/Champs de ble.jpg'],
     referee: 'LadyLag',
     date: ['21st Jan - 20:00 UTC']
   },
   {
     pseudo: ['pokerfesh'],
-    img: ['https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/placeholder.png'],
     referee: 'LadyLag',
     date: ['23rd Jan - 10:30 UTC']
   },
   {
     pseudo: ['poing', 'moosepi', 'Arbusion'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/poing.png', '../../assets/images/playerspfp/moosepi.jpg', '../../assets/images/playerspfp/Arbusion.jpg'],
     referee: 'Element118',
     date: ['25th Jan - 05:00 UTC']
   },
   {
     pseudo: ['Xellander', '0CitrineQueen0', 'KeRLi_', 'persik23', 'Samoyed'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/Xellander.jpg', '../../assets/images/playerspfp/0CitrineQueen0.png', '../../assets/images/playerspfp/placeholder.png', '../../assets/images/playerspfp/persik23.jpg', '../../assets/images/playerspfp/Samoyed.jpg'],
     referee: 'Starger',
     date: ['25th Jan - 11:00 UTC']
   },
   {
     pseudo: ['zd101nb', 'Blujae', 'Maou', 'Parax_UnDeath', 'DrugManager', 'Enohx', 'Malox', '-[ GuiboxFR ]-'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/zd101nb.jpg', '../../assets/images/playerspfp/Blujae.jpg', '../../assets/images/playerspfp/Maou.jpg', '../../assets/images/playerspfp/Parax_UnDeath.jpg', '../../assets/images/playerspfp/DrugManager.png', '../../assets/images/playerspfp/Enohx.jpg', '../../assets/images/playerspfp/Malox.png', '../../assets/images/playerspfp/-[ GuiboxFR ]-.jpg'],
     referee: 'QEpicAce',
     date: ['25th Jan - 13:00 UTC']
   },
   {
     pseudo: ['ZayyKen', 'yaityboi', 'Vasyadogs', 'Olii', 'poppasanas', 'Kozzo', 'BadAimBoi', 'awdse22'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/placeholder.png', '../../assets/images/playerspfp/yaityboi.png', '../../assets/images/playerspfp/Vasyadogs.jpg', '../../assets/images/playerspfp/Olii.jpg', '../../assets/images/playerspfp/poppasanas.gif', '../../assets/images/playerspfp/Kozzo.jpg', '../../assets/images/playerspfp/BadAimBoi.jpg', '../../assets/images/playerspfp/awdse22.png'],
     referee: 'Starger',
     date: ['25th Jan - 15:00 UTC']
   },
   {
     pseudo: ['TorreManias', 'DrDazzle', 'Senko-san', 'Ristpek', 'Zidyx', 'Lazereed'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/TorreManias.jpg', '../../assets/images/playerspfp/DrDazzle.png', '../../assets/images/playerspfp/Senko-san.jpg', '../../assets/images/playerspfp/Ristpek.jpg', '../../assets/images/playerspfp/Zidyx.jpg', '../../assets/images/playerspfp/Lazereed.jpg'],
     referee: 'QEpicAce',
     date: ['25th Jan - 17:00 UTC']
   },
   {
     pseudo: ['Dartandr', 'Boaz', 'Ullei', 'Hanik', 'Aidown', 'SergoM4444'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/Dartandr.jpg', '../../assets/images/playerspfp/Boaz.png', '../../assets/images/playerspfp/Ullei.jpg', '../../assets/images/playerspfp/Hanik.jpg', '../../assets/images/playerspfp/Aidown.jpg', '../../assets/images/playerspfp/SergoM4444.jpg'],
     referee: 'QEpicAce',
     date: ['25th Jan - 19:00 UTC']
   },
   {
     pseudo: ['HiddenDenki', 'helios44', 'Megafan', 'quadrillionaire', 'SuperSquirrel51', 'Vaf'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/HiddenDenki.jpg/', '../../assets/images/playerspfp/helios44.png', '../../assets/images/playerspfp/Megafan.jpg', '../../assets/images/playerspfp/quadrillionaire.jpg', '../../assets/images/playerspfp/SuperSquirrel51.jpg', '../../assets/images/playerspfp/Vaf.jpg'],
     referee: 'Element118',
     date: ['26th Jan - 02:00 UTC']
   },
   {
     pseudo: ['Yeong Yuseong', 'Loliezi1337'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/Yeong Yuseong.jpg', '../../assets/images/playerspfp/Loliezi1337.jpg'],
     referee: 'E-M-i',
     date: ['26th Jan - 06:00 UTC']
   },
   {
     pseudo: ['BitDust', 'Pe6eHoK98', 'Santeri', 'AhzekAhriman', 'claggole'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/BitDust.jpg', '../../assets/images/playerspfp/Pe6eHoK98.jpg', '../../assets/images/playerspfp/Santeri.png', '../../assets/images/playerspfp/AhzekAhriman.png', '../../assets/images/playerspfp/claggole.jpg'],
     referee: '	Krekker',
     date: ['26th Jan - 12:00 UTC']
   },
   {
     pseudo: ['Fre3Styl3r', 'TLQ_Yoshii', '_kingi'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/Fre3Styl3r.jpg', '../../assets/images/playerspfp/TLQ_Yoshii.jpg', '../../assets/images/playerspfp/placeholder.png'],
     referee: 'Sayira',
     date: ['26th Jan - 15:00 UTC']
   },
   {
     pseudo: ['-Storm7-', 'DieButterStulle', '-jordan-', 'Kirross', 'ByTiger', 'BeyondBoredom'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/-Storm7-.jpg', '../../assets/images/playerspfp/DieButterStulle.jpg', '../../assets/images/playerspfp/-jordan-.jpg', '../../assets/images/playerspfp/Kirross.jpg', '../../assets/images/playerspfp/ByTiger.jpg', '../../assets/images/playerspfp/placeholder.png'],
     referee: 'Krekker',
     date: ['26th Jan - 16:00 UTC']
   },
   {
     pseudo: ['Iajaygames', 'wawrzyn9', 'Katsumi', 'Corruptyouth', 'cakeboss05'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/placeholder.png', '../../assets/images/playerspfp/placeholder.png', '../../assets/images/playerspfp/Katsumi.png', '../../assets/images/playerspfp/Corruptyouth.png', '../../assets/images/playerspfp/placeholder.png'],
     referee: 'Krekker',
     date: ['26th Jan - 20:00 UTC']
   },
   {
     pseudo: ['shineasone', 'Geromu Keretem', 'Erkfir', 'NutAndGo', 'Dohland'],
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    img: ['../../assets/images/playerspfp/shineasone.jpg', '../../assets/images/playerspfp/Geromu Keretem.jpg', '../../assets/images/playerspfp/Erkfir.png', '../../assets/images/playerspfp/NutAndGo.png', '../../assets/images/playerspfp/Dohland.png'],
     referee: 'Krekker',
     date: ['26th Jan - 23:59 UTC']
   },
