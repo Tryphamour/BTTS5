90ecddb86cd71079c89d4bafa26d3768979d1db6
Tryphamour <felix.gaillard51@gmail.com>
2020-02-19 11:49:38 +0100

    Update qualifiers

--- angular/src/app/mock-lobbies.ts
+++ angular/src/app/mock-lobbies.ts
@@ -1,68 +1,101 @@
-import { Lobby } from './lobby';
+import {
+  Lobby
+} from './lobby';
 
-export const LOBBIES: Lobby[] = [
-  { pseudo: ['squidstain', 'Pickxarro'], 
-    img: ['../../assets/images/playerspfp/squidstain.jpg', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+export const LOBBIES: Lobby[] = [{
+    pseudo: ['squidstain', 'Pickxarro'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'LadyLag',
+    date: ['20th Jan - 14:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['Champs de ble'],
+    img: ['https://via.placeholder.com/75'],
+    referee: 'LadyLag',
+    date: ['21st Jan - 20:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['pokerfesh'],
+    img: ['https://via.placeholder.com/75'],
+    referee: 'LadyLag',
+    date: ['23rd Jan - 10:30 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['poing', 'moosepi', 'Arbusion'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Element118',
+    date: ['25th Jan - 05:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['Xellander', '0CitrineQueen0', 'KeRLi_', 'persik23', 'Samoyed'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Starger',
+    date: ['25th Jan - 11:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['zd101nb', 'Blujae', 'Maou', 'Parax_UnDeath', 'DrugManager', 'Enohx', 'Malox', '-[ GuiboxFR ]-'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'QEpicAce',
+    date: ['25th Jan - 13:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['ZayyKen', 'yaityboi', 'Vasyadogs', 'Olii', 'poppasanas', 'Kozzo', 'BadAimBoi', 'awdse22'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Starger',
+    date: ['25th Jan - 15:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['TorreManias', 'DrDazzle', 'Senko-san', 'Ristpek', 'Zidyx', 'Lazereed'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'QEpicAce',
+    date: ['25th Jan - 17:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['Dartandr', 'Boaz', 'Ullei', 'Hanik', 'Aidown', 'SergoM4444'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'QEpicAce',
+    date: ['25th Jan - 19:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['HiddenDenki', 'helios44', 'Megafan', 'quadrillionaire', 'SuperSquirrel51', 'Vaf'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Element118',
+    date: ['26th Jan - 02:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['Yeong Yuseong', 'Loliezi1337'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'E-M-i',
+    date: ['26th Jan - 06:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['BitDust', 'Pe6eHoK98', 'Santeri', 'AhzekAhriman', 'claggole'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: '	Krekker',
+    date: ['26th Jan - 12:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['Fre3Styl3r', 'TLQ_Yoshii', '_kingi'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Sayira',
+    date: ['26th Jan - 15:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['-Storm7-', 'DieButterStulle', '-jordan-', 'Kirross', 'ByTiger', 'BeyondBoredom'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Krekker',
+    date: ['26th Jan - 16:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
+  {
+    pseudo: ['Iajaygames', 'wawrzyn9', 'Katsumi', 'Corruptyouth', 'cakeboss05'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Krekker',
+    date: ['26th Jan - 20:00 UTC']
   },
-  { pseudo: ['John Doe', 'Peter Delarue', 'Francis Duprès', 'Red BySfr'], 
-    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
-    referee: 'LadyLag'
-  }
+  {
+    pseudo: ['shineasone', 'Geromu Keretem', 'Erkfir', 'NutAndGo', 'Dohland'],
+    img: ['https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75', 'https://via.placeholder.com/75'],
+    referee: 'Krekker',
+    date: ['26th Jan - 23:59 UTC']
+  },
 ];
