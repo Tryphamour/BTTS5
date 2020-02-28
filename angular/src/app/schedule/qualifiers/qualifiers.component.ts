f49149b3487daf5c0ff456634fff7d34dff590bf
Cory Lynch <cjl2625@gmail.com>
2020-02-28 15:20:53 -0500

    Fix layout for quals page

--- angular/src/app/schedule/qualifiers/qualifiers.component.ts
+++ angular/src/app/schedule/qualifiers/qualifiers.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { LOBBIES } from '../../mock-lobbies';
+import { LOBBIES } from "../../mock-lobbies";
 
 @Component({
-  selector: 'app-qualifiers',
-  templateUrl: './qualifiers.component.html',
-  styleUrls: ['./qualifiers.component.scss']
+  selector: "app-qualifiers",
+  templateUrl: "./qualifiers.component.html",
+  styleUrls: ["./qualifiers.component.scss"]
 })
 export class QualifiersComponent implements OnInit {
+  lobbies = [];
 
-  lobbies = LOBBIES;
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // zip together names and pfp
+    this.lobbies = LOBBIES.map(lobby => {
+      return {
+        players: lobby.pseudo.map((name, i) => ({ name, img: lobby.img[i] })),
+        ...lobby
+      };
+    });
   }
 
+  ngOnInit() {}
 }
